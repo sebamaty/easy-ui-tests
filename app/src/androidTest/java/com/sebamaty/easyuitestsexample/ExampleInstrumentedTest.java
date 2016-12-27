703d71f329c1d51cc29924761624c110ae7a95c8
Sebastian Matysiak <seba.maty@gmail.com>
2016-12-27 15:23:59 +0100

    Create an example test

--- app/src/androidTest/java/com/sebamaty/easyuitestsexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sebamaty/easyuitestsexample/ExampleInstrumentedTest.java
@@ -1,26 +1,23 @@
 package com.sebamaty.easyuitestsexample;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.sebamaty.easyuitests.UiTestsHelper;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sebamaty.easyuitestsexample", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void assertWelcomeText() throws Exception {
+        UiTestsHelper.assertTextOnViewWithId(R.id.welcome_text_view, "Hello World!");
     }
 }
+
